3dde039ea06a1c2df3f50d5823b22728a31d8735
porange <psalmon.ext@orange.com>
2014-10-07 10:42:19 +0100

    HTTP request

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 
+from httplib2 import Http
+from urllib import urlencode
+
+h = Http()
+
+
+resp, content = h.request("http://www.google.com", "GET")
+
+print(resp)
 
 print("Hello World !")
+
+
